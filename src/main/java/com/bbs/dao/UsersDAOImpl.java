c60dc392e20eda6368cd87382475128c89ca768e
revonoa <w2452510@gmail.com>
2021-11-24 09:35:12 +0900

    First commit

---
+++ src/main/java/com/bbs/dao/UsersDAOImpl.java
@@ -0,0 +1,14 @@
+package com.bbs.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UsersDAOImpl implements UsersDAO {
+
+	@Override
+	public int check_id(String user_id) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/main/java/com/bbs/service/UsersServiceImpl.java
@@ -0,0 +1,22 @@
+package com.bbs.service;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.bbs.dao.UsersDAO;
+
+@Service
+public class UsersServiceImpl implements UsersService {
+
+	@Inject
+	UsersDAO dao;
+	
+	@Override
+	public int check_id(String user_id) throws Exception {
+		// TODO Auto-generated method stub
+		dao.check_id(user_id);
+		return 0;
+	}
+
+}
